a5c3aa1260296e8ed222d5a34795bc32eb7d420a
Yusuf Yuksel <yusufyuksel@aol.com>
2024-01-05 13:29:41 +0000

    Refactor item model and tests

--- src/models/item.py
+++ src/models/item.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-
-class Item(BaseModel):
-    id: int
+from sqlmodel import SQLModel, Field
+class Item(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(...,min_length=1,max_length=100)
     description: Optional[str]= Field(None, max_length=500)
     status: bool = False
--- tests/item_test.py
+++ tests/item_test.py
@@ -2,25 +2,25 @@ import pytest
 from src.models.item import Item as item
 
 def test_item_description_return_string():
-    item1 = item(id=1,name="Sword", description="A sword for fighting")
+    item1 = item(name="Sword", description="A sword for fighting")
     assert type(item1.description) == str
 
 def test_item_name_return_string():
-    item1 = item(id=1,name="Sword", description="A sword for fighting")
+    item1 = item(name="Sword", description="A sword for fighting")
     assert type(item1.name) == str
 
 def test_item_status_return_boolean():
-    item1 = item(id=1,name="Sword", description="A sword for fighting", status=True)
+    item1 = item(name="Sword", description="A sword for fighting", status=True)
     assert type(item1.status) == bool
 
 def test_item_status_return_boolean_default_false():
-    item1 = item(id=1,name="Sword", description="A sword for fighting")
+    item1 = item(name="Sword", description="A sword for fighting")
     assert item1.status == False
 
 def test_item_name_if_empty_gives_error():
     with pytest.raises(ValueError):
-        item1 = item(id=1,name="", description="A sword for fighting")
+        item1 = item(name="", description="A sword for fighting")
 
 def test_item_has_id():
-    item1 = item(id=1,name="Sword", description="A sword for fighting")
+    item1 = item(name="Sword", description="A sword for fighting")
     assert hasattr(item1, "id") == True
