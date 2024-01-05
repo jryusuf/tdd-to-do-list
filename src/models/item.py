1703adafa276ca3b167fa18ede3d45c511b3342f
Yusuf Yuksel <yusufyuksel@aol.com>
2024-01-05 15:17:08 +0000

    Refactor item and todolist endpoints

--- src/models/item.py
+++ src/models/item.py
@@ -8,12 +8,9 @@ class ItemBase(SQLModel):
     description: Optional[str]= Field(None, max_length=500)
     status: bool = False
 
-    list_id: Optional[int] = Field(default=None, foreign_key="todolist.id")
 
 class Item(ItemBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-
-    list: Optional['ToDoList'] = Relationship(back_populates="items")
 
 class ItemCreate(ItemBase):
     pass
@@ -47,7 +44,10 @@ def get_session():
         yield session 
 
 @app.post("/item/", response_model=ItemRead)
-def create_item(item: ItemCreate):
+def create_item(
+    *,
+    session:Session = Depends(get_session),
+    item: ItemCreate):
     with Session(engine) as session:
         db_item = Item.model_validate(item)
         session.add(db_item)
@@ -71,41 +71,38 @@ def read_item(
     *,
     session:Session = Depends(get_session),
     item_id: int):
-    with Session(engine) as session:
-        item = session.get(Item, item_id)
-        if not item:
-            raise HTTPException(status_code=404, detail="Item not found")
-        return item
+    item = session.get(Item, item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
     
 @app.patch("/item/{item_id}", response_model=ItemRead)
 def update_item(
     *,
     session:Session = Depends(get_session),
     item_id: int, item: ItemUpdate):
-    with Session(engine) as session:
-        db_item = session.get(Item, item_id)
-        if not db_item:
-            raise HTTPException(status_code=404, detail="Item not found")
-        item_data = item.model_dump(exclude_unset=True)
-        for key, value in item_data.items():
-            setattr(db_item, key, value)
-        session.add(db_item)
-        session.commit()
-        session.refresh(db_item)
-        return db_item
+    db_item = session.get(Item, item_id)
+    if not db_item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    item_data = item.model_dump(exclude_unset=True)
+    for key, value in item_data.items():
+        setattr(db_item, key, value)
+    session.add(db_item)
+    session.commit()
+    session.refresh(db_item)
+    return db_item
     
 @app.delete("/item/{item_id}")
 def delete_item(
     *,
     session:Session = Depends(get_session),
     item_id: int):
-    with Session(engine) as session:
-        item = session.get(Item, item_id)
-        if not item:
-            raise HTTPException(status_code=404, detail="Item not found")
-        session.delete(item)
-        session.commit()
-        return {"ok": True}
+    item = session.get(Item, item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    session.delete(item)
+    session.commit()
+    return {"ok": True}
     
 
 
@@ -117,7 +114,6 @@ class ToDoListBase(SQLModel):
 class ToDoList(ToDoListBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
-    items: List['Item'] = Relationship(back_populates="todolist")
 
 class ToDoListCreate(ToDoListBase):
     pass
@@ -128,3 +124,25 @@ class ToDoListRead(ToDoListBase):
 class ToDoListUpdate(SQLModel):
     name: Optional[str] = Field(None,min_length=1,max_length=100)
     description: Optional[str]= Field(None, max_length=500)
+
+@app.post("/todolist/", response_model=ToDoListRead)
+def create_todolist(
+    *,
+    session:Session = Depends(get_session),
+    todolist: ToDoListCreate):
+    db_todolist = ToDoList.model_validate(todolist)
+    session.add(db_todolist)
+    session.commit()
+    session.refresh(db_todolist)
+    return db_todolist
+
+@app.get("/todolists/", response_model=list[ToDoListRead])
+def read_todolists(
+    *,
+    session:Session = Depends(get_session),
+    offset: int = 0, 
+    limit: int = Query(default=100, le=100)):
+    todolists = session.exec(select(ToDoList).offset(offset).limit(limit)).all()
+    if not todolists:
+        raise HTTPException(status_code=404, detail="ToDoLists not found")
+    return todolists
